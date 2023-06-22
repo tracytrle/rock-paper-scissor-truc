6eb31f0b48618087b50960f01b03a7308bfaa7de
Truc Tran <tracytrle@gmail.com>
2023-06-21 23:11:17 -0500

    done

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function ChoiceCard({ gameItem, handleGameItemChange }) {
   const { url, name } = gameItem;
+
   return (
     <img
       onClick={() => {
-        "Your code here";
+        handleGameItemChange(gameItem);
       }}
       className="choice"
       src={url}
